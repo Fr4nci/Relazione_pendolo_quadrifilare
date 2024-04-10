5c12481e1f9fd0279c4c8463114d754a499b1d29
Fr4nci <francesco291104@gmail.com>
2024-04-10 18:19:43 +0200

    Stesura quasi finale della relazione

--- calcolo.py
+++ calcolo.py
@@ -28,16 +28,21 @@ err_vel = np.sqrt((err_w/w) ** 2 + (err_l/l) ** 2 + (err_d/d)**2 + (err_trans_ti
 plt.plot(tempo, periodo)
 plt.savefig("Grafico_periodo_tempo.pdf")
 plt.show()
+plt.plot(tempo, vel)
 plt.savefig("Grafico_velocita_tempo.pdf")
 plt.show()
 popt, pcov = curve_fit(velocita, tempo, vel, sigma=err_vel, p0=[0.58, 0.01/5])
-plt.errorbar(tempo, vel, yerr=err_vel, fmt='o')
 v_0_hat, _lambda_hat = popt
 sigma_v_0, sigma_lamdba = np.sqrt(np.diag(pcov))
+fig = plt.figure("Fit e residui velocità")
+ax1, ax2 = fig.subplots(2, 1, sharex=True, gridspec_kw=dict(height_ratios=[2, 1], hspace=0.5))
 print(f"v_0: {v_0_hat} +- {sigma_v_0}, lambda: {_lambda_hat} +- {sigma_lamdba}")
-plt.plot(tempo, velocita(tempo, v_0_hat, _lambda_hat), color='orange')
-plt.savefig("Fit_velocita.pdf")
-plt.show()
+ax1.plot(tempo, velocita(tempo, v_0_hat, _lambda_hat), color='orange', label="Modello di best-fit")
+ax1.errorbar(tempo, vel, yerr=err_vel, xerr=err_trans_time, fmt='o', label="Dati")
+ax1.set_xlabel("t [s]")
+ax1.set_ylabel("v [m/s]")
+ax1.grid(color='lightgray', ls="dashed")
+ax1.legend()
 # Residui grafico velocità tempo
 acc = 0
 for el in range(len(vel)):
@@ -45,9 +50,12 @@ for el in range(len(vel)):
 print(acc)
 print(len(vel))
 residui = vel - velocita(tempo, v_0_hat, _lambda_hat)
-plt.errorbar(tempo, residui, yerr=err_vel, fmt='o')
-plt.axhline(y=0)
-plt.savefig("Grafico_residui_velocita.pdf")
+ax2.errorbar(tempo, residui, yerr=err_vel, fmt='o')
+ax2.axhline(y=0, color="orange")
+ax2.set_xlabel("t [s]")
+ax2.set_ylabel("Residui [m/s]")
+ax2.grid(color='lightgray', ls='dashed')
+plt.savefig("Fit_velocita.pdf")
 plt.show()
 # Sezione Taylor con due ordini di espansione differenti
 data = np.loadtxt("test4_angolo_grand_1.txt", unpack=True)
@@ -58,7 +66,7 @@ k = (w ** 2 * l)/(2 * (transit_time ** 2) * (d ** 2) * g)
 sigma_k = np.sqrt(2*(err_w/w) ** 2 + (err_l/l)** 2 + 2 * (err_trans_time/transit_time) ** 2 + 2 * (err_d/d) ** 2) * k
 theta_0 = np.arccos(1 - k)
 err_theta_0 = (1/np.sqrt(1-(1-k)**2)) * sigma_k
-err_periodo = np.full(shape=len(periodo), fill_value=0.000040, dtype=float)
+err_periodo = np.full(shape=len(periodo), fill_value=0.0001, dtype=float)
 popt, pcov = curve_fit(periodo_taylor, theta_0, periodo, sigma=err_periodo)
 t0_hat = popt
 sigma_t0 = np.sqrt(np.diag(pcov))
@@ -69,6 +77,25 @@ plt.errorbar(theta_0, periodo, yerr=err_periodo, xerr=err_theta_0, fmt='o')
 x = np.linspace(0, np.pi/2)
 plt.plot(x, periodo_taylor(x, t0_hat))
 plt.plot(x, periodo_taylor_2(x, t1_hat))
+plt.savefig("Fit_ampiezza.pdf")
 plt.show()
 print(f"{t0_hat} +- {sigma_t0}")
 print(f"{t1_hat} +- {sigma_t1}")
+residui1 = periodo - periodo_taylor(theta_0, t0_hat)
+residui2 = periodo - periodo_taylor_2(theta_0, t1_hat)
+plt.errorbar(theta_0, residui1, yerr=err_periodo, xerr=err_theta_0, fmt='o', color="blue")
+plt.axhline(y=0, color="orange")
+plt.savefig("Residui_taylor_2.pdf")
+acc = 0
+for el in range(len(periodo)):
+    acc = acc + ((periodo[el] - periodo_taylor(theta_0[el], t0_hat))/err_periodo[el]) ** 2
+print(f"Il chi quadro di Taylor al secondo ordine è: {acc}")
+plt.show()
+plt.errorbar(theta_0, residui2, yerr=err_periodo, xerr=err_theta_0, fmt='o', color="blue")
+plt.axhline(y=0, color="orange")
+plt.savefig("Residui_taylor_4.pdf")
+plt.show()
+acc = 0
+for el in range(len(periodo)):
+    acc = acc + ((periodo[el] - periodo_taylor_2(theta_0[el], t1_hat))/err_periodo[el]) ** 2
+print(f"Il chi quadro di Taylor al quarto ordine è: {acc}")
